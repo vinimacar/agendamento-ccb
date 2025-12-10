82bd536eca26c7341a183a22cb7a7b70643563a8
vinimacar <vinimacar@gmail.com>
2025-12-10 12:12:37 -0300

    Fix: Adicionar basename ao BrowserRouter para corrigir rotas no GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/agendamento-ccb">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/login" element={<Login />} />
