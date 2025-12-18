42027f75b916593a3fbc985e4f272f042c1700d1
vinimacar <vinimacar@gmail.com>
2025-12-17 21:57:20 -0300

    fix: remove proteção de login da rota /reforco-agendamento para acesso público

--- src/App.tsx
+++ src/App.tsx
@@ -117,10 +117,7 @@ const App = () => (
                 </ProtectedRoute>
               }
             />
-            <Route
-              path="/reforco-agendamento"
-              element={<ReforcoAgendamento />}
-            />
+            <Route path="/reforco-agendamento" element={<ReforcoAgendamento />} />
             <Route
               path="/rjm"
               element={
